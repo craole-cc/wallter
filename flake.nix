b9abd16afe1e032819c8f5ec6403909d0c135392
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-12-09 12:50:58 -0500

    added wayland pkg to flake

--- flake.nix
+++ flake.nix
@@ -7,19 +7,22 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    rust-overlay,
-    flake-utils,
-  }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+    }:
     flake-utils.lib.eachDefaultSystem (
-      system: let
-        overlays = [(import rust-overlay)];
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays self;
         };
-      in {
+      in
+      {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             # Rust toolchain
@@ -28,6 +31,8 @@
             # OpenSSL and dependencies
             openssl
             pkg-config
+            wayland.dev
+            libxkbcommon
 
             # Additional useful tools
             cargo-watch
